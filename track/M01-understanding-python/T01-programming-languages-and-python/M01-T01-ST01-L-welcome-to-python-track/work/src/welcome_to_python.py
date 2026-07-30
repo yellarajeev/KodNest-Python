05cec8c9c3eb1d2b94d50bfad34a6ab406d349f8
Yella Rajeev <yellarajeev@gmail.com>
2026-07-30 11:34:22 +0530

    M01-T01-ST01-L: begin the Python track

--- track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/welcome_to_python.py
@@ -1,9 +1,1 @@
-# Welcome to Python Track
-# This is your first Python program in the KodNest Python track.
-
-def main():
-    # Write your code here
-    pass
-
-if __name__ == "__main__":
-    main()
+#write you code here

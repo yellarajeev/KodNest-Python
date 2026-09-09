35e088b4bb0a750eb81fa2f5c53d2f47ee934e02
yellarajeev <yellarajeev@gmail.com>
2026-09-09 14:52:36 +0530

    Added Python programs

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,20 @@
+print(" this is my First Program")
+
+#Assignment: Basic print() in Python
+#Task: Write a Python program that prints the following information:
+#Your name
+#Your age
+#Your city
+#Your favorite programming language
+#The message: In a paragraph about your 1 week of python Journey."
+print("Barinepalle Ashish")
+print("22")
+print("bengaluru")
+print("My favorite language is Python")
+print("it's been my 1st week of my python journey i started knowing how python is different from the other programming language, it's been fun learning python ")
+#Task: Print the following pattern using only print()
+print("*")
+print("**")
+print("***")
+print("****")
+print("******")

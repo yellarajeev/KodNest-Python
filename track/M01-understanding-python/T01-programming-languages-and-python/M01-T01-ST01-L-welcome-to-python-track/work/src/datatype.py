2fefb425b9a191c5ba39f384a953c4af394e2206
Yella Rajeev <yellarajeev@gmail.com>
2026-09-10 15:48:20 +0530

    Completed variables and datatypes practice

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/datatype.py
@@ -0,0 +1,16 @@
+# primitive data types
+age = 21
+print(age, type(age))
+
+marks = 45.1
+print(marks, type(marks))
+
+comment= "A"
+print(comment, type(comment))
+
+has_job= False
+print(has_job, type(has_job))
+
+cnum = 3 + 7j
+print(cnum, type(cnum))
+#List, tuple, set, dictionary
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variables.py
@@ -0,0 +1,20 @@
+name = "rajeev "
+name = "ganesh"
+print("name")
+
+age = "22"
+
+# my name is ganesh and age 22 !
+print("my name is" + name + "and age is", age, "!")
+print("my name is" + name + " and age is" + str(age) + "!")
+
+# formatting strings - f
+print(f"my name is {name} and age is {age}!")
+
+mark = 50
+mark = mark + 10
+print(mark)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+print(a + b + c)

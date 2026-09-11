cf9c06625dcc9d7e1585c2c1f4545f20c2bdf8a9
Yella Rajeev <yellarajeev@gmail.com>
2026-09-11 15:08:44 +0530

    Completed input practice

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/input.py
@@ -0,0 +1,13 @@
+#name = "rajeev"
+name = input("Enter the name:")
+print(f"My name is {name} !")
+
+age = input("Enter the age")
+print(f"tHIS IS {age}")
+
+#value input -> str
+#add 2 numbers
+a = int(input("a :"))
+b = int(input("b :"))
+c = a + b
+print(c)
